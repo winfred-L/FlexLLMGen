51027a418f70e3a7aac6af53153cd89f867d9fc3
winfred <winfredL@outlook.com>
2025-12-12 03:33:54 +0000

    sync

--- flexllmgen/pytorch_backend.py
+++ flexllmgen/pytorch_backend.py
@@ -1322,30 +1322,32 @@ class TorchMixedDevice:
 
 class TorchLink:
     """An I/O link between two devices."""
+    # unused
+    pass
 
-    def __init__(self, a, b, a_to_b_bandwidth, b_to_a_bandwidth):
-        self.a = a
-        self.b = b
-        self.a_to_b_bandwidth = a_to_b_bandwidth
-        self.b_to_a_bandwidth = b_to_a_bandwidth
+    # def __init__(self, a, b, a_to_b_bandwidth, b_to_a_bandwidth):
+    #     self.a = a
+    #     self.b = b
+    #     self.a_to_b_bandwidth = a_to_b_bandwidth
+    #     self.b_to_a_bandwidth = b_to_a_bandwidth
 
-        a.add_link(self)
-        b.add_link(self)
+    #     a.add_link(self)
+    #     b.add_link(self)
 
-    def io_time(self, src, dst, size):
-        if src == self.a:
-            assert dst == self.b
-            bandwidth = self.a_to_b_bandwidth
-        elif src == self.b:
-            assert dst == self.a
-            bandwidth = self.b_to_a_bandwidth
-        else:
-            raise ValueError(f"Invalid source {src}")
+    # def io_time(self, src, dst, size):
+    #     if src == self.a:
+    #         assert dst == self.b
+    #         bandwidth = self.a_to_b_bandwidth
+    #     elif src == self.b:
+    #         assert dst == self.a
+    #         bandwidth = self.b_to_a_bandwidth
+    #     else:
+    #         raise ValueError(f"Invalid source {src}")
 
-        if force_io_time is not None:
-            return force_io_time
+    #     if force_io_time is not None:
+    #         return force_io_time
 
-        return size / bandwidth
+    #     return size / bandwidth
 
 
 def general_copy(dst: TorchTensor, dst_indices: Tuple[slice],
